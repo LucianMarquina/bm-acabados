52792121fdb82ada3547d36ce063b656378ec63c
LucianMarquina <alemarquina789@gmail.com>
2025-12-10 22:54:47 -0500

    feat: Primeras integraciones UI y configuraciones externas listas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css}'],
-    theme: {
-        extend: {
-            colors: {
-                'goldenPrimary': '#D4AF37',
-                'goldenSecondary': '#E7C468',
-                'bluePrimary': '#1E40AF',
-                'blueDark': '#1A304E',
-                'blueText': '#002251',
-                'darkText': '#4B4B4B',
-                'lightText': '#E8E8E8',
-                'whiteBg': '#F5F5F5',
-                'blackBg': '#1A1A1A'
-            },
-        },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css}"],
+  theme: {
+    extend: {
+      colors: {
+        goldenPrimary: "#D4AF37",
+        goldenSecondary: "#E7C468",
+        bluePrimary: "#1E40AF",
+        blueDark: "#1A304E",
+        blueText: "#002251",
+        darkText: "#4B4B4B",
+        lightText: "#E8E8E8",
+        whiteBg: "#F5F5F5",
+        blackBg: "#1A1A1A",
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
